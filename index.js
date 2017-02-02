23b6d787c1a7d4e53475f1236d572b9c78a2084d
Klaus l. Hougesen <klh@threads.dk>
2017-02-02 10:58:09 +0100

    working version - change credentials

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const Appdyn = require('./lib/appdynamics_scraper')
-var client = new Appdyn({user: 'teamyousee@TDCAS', pass: 'Yousee1234'}, this.getData)
-
-var dbFailedPages, dbJsErrors
+var client = new Appdyn({user: 'USERNAME@HOST', pass: 'PASSWORD'}, getData)
 
 // Get list of top pages with js errors
 
 function getData () {
-  console.log('ddd')
-  client.getTopPages({sortBy: 'totalPageViewWithJavaScriptError', appId: 2702})
+  client.getTopPages({sortBy: 'totalPageViewWithJavaScriptError', appId: 2702,numToShow:25})
+  client.getTopAnalytics({days: 5, size: 10,pages:[
+                            "selvbetjening.yousee.dk",
+                            "selvbetjening.yousee.dk/regninger",
+                            "selvbetjening.yousee.dk/tv",
+                            "selvbetjening.yousee.dk/regninger/forbrugsdetaljer/downloadpdf",
+                            "selvbetjening.yousee.dk/regninger/forbrugsdetaljer",
+                            "selvbetjening.yousee.dk/mobil",
+                            "selvbetjening.yousee.dk/indstillinger",
+                            "selvbetjening.yousee.dk/bredbaand",
+                            "selvbetjening.yousee.dk/fastnet",
+                            "selvbetjening.yousee.dk/fordele",
+                            "selvbetjening.yousee.dk/mobiltbredbaand"
+                          ]})
 }
